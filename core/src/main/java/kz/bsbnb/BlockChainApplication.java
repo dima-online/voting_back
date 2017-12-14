a2fb2573d7fe0b8d182c4401b6b510921fa683ab
Olzhas.Pazyldayev <olzhas.pazyldayev@bsbnb.kz>
2017-12-14 17:57:49 +0600

    +main class revert

--- core/src/main/java/kz/bsbnb/BlockChainApplication.java
+++ core/src/main/java/kz/bsbnb/BlockChainApplication.java
@@ -18,7 +18,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import java.util.Locale;
 
 @SpringBootApplication
-//@EnableSwagger2
+@EnableSwagger2
 
 @Configuration
 @EnableAutoConfiguration
@@ -32,7 +32,7 @@ public class BlockChainApplication {
         //CryptUtil.signXML("<test></test>","C:\\Users\\Serik.Mukashev\\Documents\\voting\\RSA256_cff149024ec3c1bc05abb2bf7a4780e29ee692bc.p12","123456");
     }
 
-    /*@Bean
+    @Bean
     public Docket swaggerSettings() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
@@ -40,7 +40,7 @@ public class BlockChainApplication {
                 .paths(PathSelectors.any())
                 .build()
                 .pathMapping("/");
-    }*/
+    }
 
     @Bean
     public LocaleChangeInterceptor localeChangeInterceptor() {
